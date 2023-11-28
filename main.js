c0f2193eb385bb5c94c7277ceab1296c14853fc7
Wendo mbijiwe <mbijiweimunde@gmail.com>
2023-11-28 13:18:13 +0300

    Revert "revert"
    This reverts commit 4a701bef2e7ecfd96fd212dc284c25de2a7d6a6b.

--- main.js
+++ main.js
@@ -1,60 +1,144 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
+
+import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
+
 
 class BasicWorldDemo {
-    constructor() {
-        this._Initialize();
+  constructor() {
+    this._Initialize();
+  }
+
+  _Initialize() {
+    this._threejs = new THREE.WebGLRenderer({
+      antialias: true,
+    });
+    this._threejs.shadowMap.enabled = true;
+    this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
+    this._threejs.setPixelRatio(window.devicePixelRatio);
+    this._threejs.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(this._threejs.domElement);
+
+    window.addEventListener('resize', () => {
+      this._OnWindowResize();
+    }, false);
+
+    const fov = 60;
+    const aspect = 1920 / 1080;
+    const near = 1.0;
+    const far = 1000.0;
+    //creating perspective cam
+    this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this._camera.position.set(75, 20, 0);
+
+   //creating a 3d scene
+    this._scene = new THREE.Scene();
+    //Adding Directional Light to the Scene:
+    let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+    light.position.set(20, 100, 10);
+    light.target.position.set(0, 0, 0);
+    light.castShadow = true;
+    light.shadow.bias = -0.001;
+    light.shadow.mapSize.width = 2048;
+    light.shadow.mapSize.height = 2048;
+    light.shadow.camera.near = 0.1;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.near = 0.5;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.left = 100;
+    light.shadow.camera.right = -100;
+    light.shadow.camera.top = 100;
+    light.shadow.camera.bottom = -100;
+    this._scene.add(light);
+    //Adding Ambient Light to the Scene:
+    light = new THREE.AmbientLight(0x101010);
+    this._scene.add(light);
+    //Adding OrbitControls for Camera Manipulation:
+    const controls = new OrbitControls(
+      this._camera, this._threejs.domElement);
+    controls.target.set(0, 20, 0);
+    controls.update();
+    //Loading Cube Texture for Scene Background:
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.load([
+        './resources/posx.jpg',
+        './resources/negx.jpg',
+        './resources/posy.jpg',
+        './resources/negy.jpg',
+        './resources/posz.jpg',
+        './resources/negz.jpg',
+    ]);
+    this._scene.background = texture;
+    //Creating and Adding a Plane to the Scene
+    const plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(100, 100, 10, 10),
+        new THREE.MeshStandardMaterial({
+            color: 0xFFFFFF,
+          }));
+    plane.castShadow = false;
+    plane.receiveShadow = true;
+    plane.rotation.x = -Math.PI / 2;
+    this._scene.add(plane);
+    //Creating and Adding a Box to the Scene:
+    const box = new THREE.Mesh(
+      new THREE.BoxGeometry(2, 2, 2),
+      new THREE.MeshStandardMaterial({
+          color: 0xFFFFFF,
+      }));
+    box.position.set(0, 1, 0);
+    box.castShadow = true;
+    box.receiveShadow = true;
+    this._scene.add(box);
+      //Creating and Adding Randomly Positioned Boxes to the Scene:
+    for (let x = -8; x < 8; x++) {
+      for (let y = -8; y < 8; y++) {
+        const box = new THREE.Mesh(
+          new THREE.BoxGeometry(2, 2, 2),
+          new THREE.MeshStandardMaterial({
+              color: 0x808080,
+          }));
+        box.position.set(Math.random() + x * 5, Math.random() * 4.0 + 2.0, Math.random() + y * 5);
+        box.castShadow = true;
+        box.receiveShadow = true;
+        this._scene.add(box);
+      }
     }
 
-    _Initialize() {
-        this._threejs = new THREE.WebGLRenderer({
-            antialias: true,
-        });
-        // ... (rest of the initialization code)
+    // const box = new THREE.Mesh(
+    //   new THREE.SphereGeometry(2, 32, 32),
+    //   new THREE.MeshStandardMaterial({
+    //       color: 0xFFFFFF,
+    //       wireframe: true,
+    //       wireframeLinewidth: 4,
+    //   }));
+    // box.position.set(0, 0, 0);
+    // box.castShadow = true;
+    // box.receiveShadow = true;
+    // this._scene.add(box);
+    
+    //Rendering the Scene:
+    this._RAF();
+  }
 
-        window.addEventListener('resize', () => {
-            this._OnWindowResize();
-        }, false);
+  //Handling Window Resize
+  _OnWindowResize() {
+    this._camera.aspect = window.innerWidth / window.innerHeight;
+    this._camera.updateProjectionMatrix();
+    this._threejs.setSize(window.innerWidth, window.innerHeight);
+  }
 
-        // ... (rest of the initialization code)
-
-        // Add a button to start the game
-        this._AddStartButton();
-
-        // ... (rest of the initialization code)
-
-        this._RAF();
-    }
-
-    // ... (rest of the methods)
-
-    _AddStartButton() {
-        const startScreen = document.getElementById('start-screen');
-
-        const startButton = document.createElement('button');
-        startButton.textContent = 'Start Game';
-        startButton.onclick = () => this._StartGame();
-
-        startScreen.appendChild(startButton);
-    }
-
-    _StartGame() {
-        // Hide the start screen
-        document.getElementById('start-screen').style.display = 'none';
-
-        // Perform any additional setup or game initialization here
-        console.log('Game started!');
-    }
-
-    // ... (rest of the methods)
+  //Rendering Animation Frame:Recursively calls the rendering loop using requestAnimationFrame, updating the scene in each frame.
+  _RAF() {
+    requestAnimationFrame(() => {
+      this._threejs.render(this._scene, this._camera);
+      this._RAF();
+    });
+  }
 }
 
+//Creating an Instance of the Class on DOM Content Load
 let _APP = null;
 
 window.addEventListener('DOMContentLoaded', () => {
-    _APP = new BasicWorldDemo();
+  _APP = new BasicWorldDemo();
 });
-function _StartGame() {
-  // You can add the logic here to initialize the game when the Start Game button is clicked
-  console.log("Game started!");
-}
